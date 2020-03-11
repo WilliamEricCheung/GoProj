a0faa85e601b57682d53ab24976c7178f9a504ce
williamericcheung <williamericcheung@gmail.com>
2020-03-11 00:23:14 -0400

    studying

---
+++ src/ch2/echo4/main.go
@@ -0,0 +1,34 @@
+// Echo4 prints its command-line arguments.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
+
+/**
+$ go build gopl.io/ch2/echo4
+$ ./echo4 a bc def
+a bc def
+$ ./echo4 -s / a bc def
+a/bc/def
+$ ./echo4 -n a bc def
+a bc def$
+$ ./echo4 -help
+Usage of ./echo4:
+  -n    omit trailing newline
+  -s string
+        separator (default " ")
+*/
---
+++ src/ch2/ftoc/main.go
@@ -0,0 +1,19 @@
+// Ftoc prints two Fahrenheit-to-Celsius conversions.
+package main
+
+import "fmt"
+
+func main() {
+	const freezingF, boilingF = 32.0, 212.0
+	fmt.Printf("%g°F = %g°C\n", freezingF, fToC(freezingF)) // "32°F = 0°C"
+	fmt.Printf("%g°F = %g°C\n", boilingF, fToC(boilingF))   // "212°F = 100°C"
+	x := 1
+	p := &x         // p, of type *int, points to x
+	fmt.Println(*p) // "1"
+	*p = 2          // equivalent to x = 2
+	fmt.Println(x)  // "2"
+}
+
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
